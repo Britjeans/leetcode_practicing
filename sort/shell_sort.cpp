558a115798b6d76c063a197e633e96136d2977c7
Britjeans <bridgetzchina@gmail.com>
2018-04-26 17:01:51 +0800

    Create shell_sort.cpp

---
+++ sort/shell_sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void shellsort(int v[],int n)
+{
+    int gap,i,j,temp;
+    for (gap = n/2; gap > 0; gap /= 2)
+    {
+        for(i = gap; i < n; i++)
+            
+            for(j = i-gap; j>=0 && v[j]>v[j+gap]; j -= gap)
+            {
+                temp = v[j];
+                v[j] = v[j+gap];
+                v[j+gap] = temp;
+            }
+        
+    }
+}
+
+int main()
+{
+    int s[] = {9,6,3,4,5,7,8};
+    
+    shellsort(s,sizeof(s)/sizeof(int));
+    
+    for (int i=0;i<sizeof(s)/sizeof(int);i++)
+    {
+        cout<<s[i]<<" ";
+    }
+    
+    return 0;
+}
