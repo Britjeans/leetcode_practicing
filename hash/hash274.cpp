10ce0a05ed6eb05711883a6ab14d414ec50f4fc7
Wanying Ding <bridgetzchina@gmail.com>
2018-06-02 17:46:01 +0800

    Create hash274.cpp

---
+++ hash/hash274.cpp
@@ -0,0 +1,25 @@
+//
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if(citations.size()==0) return 0;
+        int min=INT_MAX,max=0,max_h=citations.size();
+        bool mark=false;
+        for(int i=0;i<citations.size();++i){
+            if(citations[i]>max) max=citations[i];
+            if(citations[i]<min) min=citations[i];          
+        }
+        for(int h=min;h<=max;++h){
+            int count=0,self=0;
+            for(int i=0;i<citations.size();++i){
+                if(citations[i]!=h)
+                    count+=(citations[i]+1)>(h+1);
+                else
+                    self+=1;
+            }
+            if((count+self)>=h&&count<=h){max_h=h;mark=true;} 
+            if(mark&&max_h!=h) return max_h;
+        }
+        return max_h;
+    }
+};
