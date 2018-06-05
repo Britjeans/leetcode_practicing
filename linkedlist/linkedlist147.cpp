b134b215ce1c5e671fd0d6ab7cd34b9c71ee6536
Wanying Ding <bridgetzchina@gmail.com>
2018-06-05 09:51:08 +0800

    Create linkedlist147.cpp

---
+++ linkedlist/linkedlist147.cpp
@@ -0,0 +1,65 @@
+//slow version
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(head==NULL||head->next==NULL) return head;
+        stack<ListNode*> s;
+        ListNode* res=NULL;
+        int count=1,i=1;
+        s.push(head);
+        head=head->next;
+        while(head!=NULL){
+            ++count;
+            ++i;
+            int val=s.top()->val;
+            if(val<=head->val) {
+                s.push(head);
+                head=head->next;
+                continue;
+            }
+            s.top()->next=head->next;           
+            while(!s.empty()){
+                res=s.top();
+                int val=s.top()->val;
+                if(val<=head->val) {
+                    head->next=s.top()->next;
+                    s.top()->next=head;
+                    s.push(head);
+                    while(i<count){
+                        ++i;
+                        head=head->next;
+                        s.push(head);                      
+                    }
+                    break;
+                }
+                s.pop();
+                --i;
+            }
+            if(s.empty()){
+                head->next=res;
+                s.push(head);
+                while(i<count){
+                        ++i;
+                        head=head->next;
+                        s.push(head);                      
+                }
+            }
+
+            head=head->next;
+        }
+        
+        while(!s.empty()){
+            res=s.top();
+            s.pop();
+        }
+        return res;
+    }
+};
